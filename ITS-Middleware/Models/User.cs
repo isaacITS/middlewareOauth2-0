4049243532231c8c9dac71255d383edec6006113
Isaac Gallegos <jesusisaac360@gmail.com>
2022-05-23 10:43:18 -0500

    Agregar archivos de proyecto.

---
+++ ITS-Middleware/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ITS_Middleware.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
