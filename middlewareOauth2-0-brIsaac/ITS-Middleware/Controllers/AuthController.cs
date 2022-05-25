75b3dcee27e6e0573ee3957e1ef954a194f0aa9e
RafaelBonRom14 <105802573+RafaelBonRom14@users.noreply.github.com>
2022-05-25 10:29:37 -0500

    25/05/2022 Cambios de Isaac (no míos)
    Versión de Isaac sin modificar
    25/05/2022

---
+++ middlewareOauth2-0-brIsaac/ITS-Middleware/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+﻿using ITS_Middleware.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ITS_Middleware.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        private AccountService accountService;
+
+        public AuthController(AccountService acntService)
+        {
+            accountService = acntService;
+        }
+
+        [Route("")]
+        [Route("~/")]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(string email, string pass)
+        {
+            var user = accountService.Login(email, pass);
+            if (user == null)
+            {
+                ViewBag.msg = "Invalid Email or Password";
+                return View("login");
+            }
+            HttpContext.Session.SetString("userEmail", email);
+            return RedirectToAction("home");
+        }
+
+        [Route("home")]
+        public IActionResult Home()
+        {
+            ViewBag.email = HttpContext.Session.GetString("userEmail");
+            return View("home");
+        }
+
+
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("userEmail");
+            return View("login");
+        }
+    }
+}
---
+++ middlewareOauth2-0-brIsaac/ITS-Middleware/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ITS_Middleware.Models
+{
+    public class User
+    {
+        public string email { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
