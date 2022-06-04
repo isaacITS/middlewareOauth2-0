42d69026139bd8e977a2b52afe6efa9452f744ad
RafaelBonRom14 <105802573+RafaelBonRom14@users.noreply.github.com>
2022-06-04 16:42:49 -0500

    Mi Login> Archivos de Isaac
    agregado los archivos de isaac y mi login
    04-06-2022
    
    no RAR

---
+++ ITS-Middleware/ITS-Middleware/Services/AccountService.cs
@@ -0,0 +1,9 @@
+﻿using ITS_Middleware.Models;
+
+namespace ITS_Middleware.Services
+{
+    public interface AccountService
+    {
+        public User Login(string email, string password);
+    }
+}
---
+++ ITS-Middleware/ITS-Middleware/UserInput.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ITS_Middleware
+{
+    
+    public class UserInput
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
