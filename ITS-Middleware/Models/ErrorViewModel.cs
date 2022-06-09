e5557a71190e5631af66e5b8670ddb736d68a567
Ulises Mtz <93839145+Rck23@users.noreply.github.com>
2022-06-08 20:20:09 -0500

    Delete ITS-Middleware directory

--- ITS-Middleware/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ITS_Middleware.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
