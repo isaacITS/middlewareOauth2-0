c6ca5d021c659e1a5fc9fb59aa57209ed0c5f570
Ulises Mtz <93839145+Rck23@users.noreply.github.com>
2022-06-17 19:54:26 -0500

    Add files via upload
    Api MIDDLEWARE, vista y loggin, si las credenciales son correctas retorna un token JWT

---
+++ API_MIDDLEWARE/Controllers/UsersController.cs
@@ -0,0 +1,89 @@
+﻿using API_MIDDLEWARE.Models;
+using API_MIDDLEWARE.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using API_MIDDLEWARE.Tools;
+using System.Security.Cryptography;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace API_MIDDLEWARE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        public middlewareITSContext _context;
+        private readonly IConfiguration _configuration;
+
+
+        public UsersController(middlewareITSContext master, ILogger<UsersController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _context = master;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Usuarios>>> Get()
+        {
+            
+            return Ok(await _context.Usuarios.ToListAsync());
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login(string email, string pass)
+        {
+            var user = _context.Usuarios.Where(u => u.Email == email).FirstOrDefault();
+            if (user == null)
+            {
+                return BadRequest("Usuario no registrado");
+            }
+            if (!user.Activo)
+            {
+                return BadRequest("Usuario no Encontrado");
+            }
+            if (user.Pass != Encrypt.GetSHA256(pass))
+            {
+                return BadRequest("Contraseña incorrecta");
+            }
+       
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+
+        
+        private string CreateToken(Usuarios user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Nombre),
+                //new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+            _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+
+
+
+    }
+}
---
+++ API_MIDDLEWARE/Models/Context/middlewareITSContext.cs
@@ -0,0 +1,77 @@
+﻿using API_MIDDLEWARE.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_MIDDLEWARE.Models.Context
+{
+    public partial class middlewareITSContext : DbContext
+    {
+        public middlewareITSContext()
+        {
+        }
+
+        public middlewareITSContext(DbContextOptions<middlewareITSContext> options)
+            : base(options)
+        {
+        }
+
+       
+        public virtual DbSet<Usuarios> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=LENOVO-PC;Initial Catalog=middlewareITS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.ToTable("usuarios");
+
+                entity.HasIndex(e => e.Email, "UQ__usuarios__AB6E6164A123E0D5")
+                    .IsUnique();
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Activo).HasColumnName("activo");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(60)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.FechaAlta)
+                    .HasColumnType("datetime")
+                    .HasColumnName("fechaAlta");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+
+                entity.Property(e => e.Pass)
+                    .HasMaxLength(150)
+                    .IsUnicode(false)
+                    .HasColumnName("pass");
+
+                entity.Property(e => e.Puesto)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("puesto");
+
+              /*  entity.Property(e => e.TokenRecovery)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("tokenRecovery");*/
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder); 
+    }
+}
